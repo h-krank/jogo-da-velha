347031bad99b85f90c5105dc30339f98eed59cde
hkrank <breno.braganca@gmail.com>
2020-08-31 12:31:35 -0300

    Updating files

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # jogo-da-velha
-Simples jogo da velha feito com HTML, CSS3 e JavaScript
+Simples jogo da velha feito com HTML, CSS3 e JavaScript com uma IA básica utilizando o algoritimo de busca minimax e otimizado com alpha-beta pruning.
 
-Este jogo foi feito para práticar com aos alunos lógica de programação com HTML5, CSS3 e JavaScript.
-Tudo foi feito de maneira bem simples.
+Este jogo usou como base o exemplo contruido pelo professor durante o Serratec para praticar com os alunos lógica de programação com HTML5, CSS3 e JavaScript.
--- js/velha.js
+++ js/velha.js
@@ -2,11 +2,11 @@
 (()=>{
 
     // Declarando variáveis e objetos utilizados para o jogo.
-    const TEXTO_USUARIO_X = 'Usuário X:  ';
-    const TEXTO_USUARIO_O = 'Usuário O:  ';
+    const X = 'X';
+    const O = 'O';
 
-    let jogadorAtual = {};
-    let jogadas = [];
+    let jogador;
+    let board = intialState();
 
     let jogo = {
         jogada1: document.querySelector('.jogo-velha-1'),
@@ -20,209 +20,266 @@
         jogada9: document.querySelector('.jogo-velha-9')
     }
 
-    let jogadorX = {
-        nome:'',
-        valor: 'X',
-    }
-
-    let jogadorO = {
-        nome:'',
-        valor: 'O'
-    }
-
     var opcoes = {
         divOpcoesJogo : document.querySelector('.opcoes-jogo'),
-        usuarioX : document.getElementById('usuario-x'),
-        usuarioO : document.getElementById('usuario-o'),
-        btnJogar : document.getElementById('btn-jogar')
-    };
-
-    var painel = {
-        painelOpcoesJogo : document.querySelector('.painel-opcoes'),
-        nomeX : document.getElementById('painel-usuario-x-nome'),
-        nomeO : document.getElementById('painel-usuario-o-nome'),
-        pontosX : document.getElementById('painel-usuario-x-pontos'),
-        pontosO : document.getElementById('painel-usuario-o-pontos'),
-        nomeProximoJogador: document.getElementById('proximo-jogador')
+        btnJogarX : document.getElementById('btn-jogar-x'),
+        btnJogarO : document.getElementById('btn-jogar-o'),
+        btnReiniciar: document.getElementById('btn-reiniciar'),
+        proximoJogar : document.querySelector('.proximo-jogar')
     };
 
     // Capturando os eventos de click
-    opcoes.btnJogar.addEventListener('click', () => {
+    opcoes.btnJogarX.addEventListener('click', () => {
+        jogador = X;
 
-        jogadorX.nome = opcoes.usuarioX.value;
-        jogadorO.nome = opcoes.usuarioO.value;
-
-        if(!jogadorX.nome || !jogadorO.nome){
-            alert('Favor informar os usuarios X e O para iniciar o jogo.');
-            return;
-        }
-
-        // Alterar os nomes dos jogadores...
-        painel.nomeX.textContent = TEXTO_USUARIO_X + jogadorX.nome;
-        painel.nomeO.textContent = TEXTO_USUARIO_O + jogadorO.nome;
-
-        jogadorAtual = jogadorX;
-        painel.nomeProximoJogador.textContent = jogadorAtual.nome;
-
-        // Aqui tenho que esconder as opções e mostrar o painel.
+        // Esconde as opções e mostrar o texto do jogador.
         opcoes.divOpcoesJogo.classList.add('esconder');
-        painel.painelOpcoesJogo.classList.remove('esconder');
+        opcoes.proximoJogar.textContent = `Jogar como ${jogador}`;
+    });
+    
+    opcoes.btnJogarO.addEventListener('click', () => {
+        jogador = O;
+
+        // Esconde as opções e inicia com a AI.
+        opcoes.divOpcoesJogo.classList.add('esconder');
+        _marcarJogada(minimax(board));
+    });
+
+    //Retorna o jogo para o estado original
+    opcoes.btnReiniciar.addEventListener('click', () => {
+        board = intialState();
+        _atualizarJogo();
+        opcoes.btnReiniciar.classList.add('esconder');
+        opcoes.proximoJogar.textContent = '';
+        opcoes.divOpcoesJogo.classList.remove('esconder');
     });
 
 
     jogo.jogada1.addEventListener('click', (e)=>{
-        jogada(e, 1);
+        jogada(e, [0, 0]);
     });
 
     jogo.jogada2.addEventListener('click', (e)=>{
-        jogada(e, 2);
+        jogada(e, [0, 1]);
     });
 
     jogo.jogada3.addEventListener('click', (e)=>{
-        jogada(e, 3);
+        jogada(e, [0, 2]);
     });
 
     jogo.jogada4.addEventListener('click', (e)=>{
-        jogada(e, 4);
+        jogada(e, [1, 0]);
     });
 
     jogo.jogada5.addEventListener('click', (e)=>{
-        jogada(e, 5);
+        jogada(e, [1, 1]);
     });
 
     jogo.jogada6.addEventListener('click', (e)=>{
-        jogada(e, 6);
+        jogada(e, [1, 2]);
     });
 
     jogo.jogada7.addEventListener('click', (e)=>{
-        jogada(e, 7);
+        jogada(e, [2, 0]);
     });
 
     jogo.jogada8.addEventListener('click', (e)=>{
-        jogada(e, 8);
+        jogada(e, [2, 1]);
     });
 
     jogo.jogada9.addEventListener('click', (e)=>{
-        jogada(e, 9);
+        jogada(e, [2, 2]);
     });
-
 
     // Funções utilizadas no jogo
 
-    function  validarJogada(){
-       let valor = jogadorAtual.valor;
-
-       if(_estrategia_1(valor) ||
-          _estrategia_2(valor) || 
-          _estrategia_3(valor) ||
-          _estrategia_4(valor) ||
-          _estrategia_5(valor) ||
-          _estrategia_6(valor) ||
-          _estrategia_7(valor) ||
-          _estrategia_8(valor)){
-
-            return true;
-        }
-
-        return false;      
-    }
-
-    function _estrategia_1(valor){
-        return (jogadas[1] == valor && jogadas[2] == valor && jogadas[3] == valor);
-    }
-    function _estrategia_2(valor){
-        return (jogadas[4] == valor && jogadas[5] == valor && jogadas[6] == valor);
-    }
-    function _estrategia_3(valor){
-        return (jogadas[7] == valor && jogadas[8] == valor && jogadas[9] == valor);
-    }
-    function _estrategia_4(valor){
-        return (jogadas[1] == valor && jogadas[4] == valor && jogadas[7] == valor);
-    }
-    function _estrategia_5(valor){
-        return (jogadas[2] == valor && jogadas[5] == valor && jogadas[8] == valor);
-    }
-    function _estrategia_6(valor){
-        return (jogadas[3] == valor && jogadas[6] == valor && jogadas[9] == valor);
-    }
-    function _estrategia_7(valor){
-        return (jogadas[1] == valor && jogadas[5] == valor && jogadas[9] == valor);
-    }
-    function _estrategia_8(valor){
-        return (jogadas[3] == valor && jogadas[5] == valor && jogadas[7] == valor);
-    }
-
-
-    function _marcarJogada(e, indice){
-        e.target.textContent = jogadorAtual.valor;
-        jogadas[indice] = jogadorAtual.valor;
-    }
-
-    function jogada(e, indice){
-
-        if(!jogadorX.nome || !jogadorO.nome || e.target.textContent){
-            return;
-        }
-
-        _marcarJogada(e, indice);
-
-
-        if(validarJogada()){
-            setTimeout(()=>{
-                alert(`Parabéns, jogador ${jogadorAtual.nome} acaba de marcar ponto.`);
-                _atualizarPainel();
-                _reiniciarJogo();
-
-            },100)
-          
-            return;
-        }
-
-        if(jogadas.filter(e => e).length == 9){
-
-            setTimeout(()=>{
-                alert('Deu velha ...');
-                _reiniciarJogo();
-            },100)
-  
-            return;
-        }
+    function _marcarJogada(action){
+        currentPlayer = player(board);
+        x = action[0]
+        y = action[1]
+        board[x][y] = currentPlayer;
+        _atualizarJogo();
+        opcoes.proximoJogar.textContent = `Jogar como ${jogador}`;
         
-        // Continuar jogando.
-        jogadorAtual = (jogadorAtual == jogadorX) ? jogadorO : jogadorX;
-
-        painel.nomeProximoJogador.textContent = jogadorAtual.nome;
+        if (terminal(board))          
+            gameOver();
     }
 
-    function _reiniciarJogo(){
+    function jogada(e, boardIndex){
 
-        jogadas = [];
-        jogo.jogada1.textContent = '';
-        jogo.jogada2.textContent = '';
-        jogo.jogada3.textContent = '';
-        jogo.jogada4.textContent = '';
-        jogo.jogada5.textContent = '';
-        jogo.jogada6.textContent = '';
-        jogo.jogada7.textContent = '';
-        jogo.jogada8.textContent = '';
-        jogo.jogada9.textContent = '';
-    }
-
-    function _atualizarPainel(){
-
-        let pontos;
-
-        if(jogadorAtual.valor == 'X'){
-            pontos = parseInt(painel.pontosX.textContent);
-            pontos++;
-            painel.pontosX.textContent = pontos;
-
+        if(e.target.textContent || !jogador){
             return;
         }
 
-        pontos  = parseInt(painel.pontosO.textContent);
-        pontos++;
-        painel.pontosO.textContent = pontos;
+        _marcarJogada(boardIndex);
+
+        if (!terminal(board)){
+            opcoes.proximoJogar.textContent = "Computador pensando...";
+            setTimeout(() => {
+                _marcarJogada(minimax(board))    
+            }, 300); 
+        }
     }
- 
+
+    function _atualizarJogo(){
+        jogo.jogada1.textContent = board[0][0];
+        jogo.jogada2.textContent = board[0][1];
+        jogo.jogada3.textContent = board[0][2];
+        jogo.jogada4.textContent = board[1][0];
+        jogo.jogada5.textContent = board[1][1];
+        jogo.jogada6.textContent = board[1][2];
+        jogo.jogada7.textContent = board[2][0];
+        jogo.jogada8.textContent = board[2][1];
+        jogo.jogada9.textContent = board[2][2];
+    }
+
+    function gameOver(){
+        jogador = null;
+        opcoes.btnReiniciar.classList.remove('esconder');
+        let vencedor = winner(board);
+        if (!vencedor){
+            opcoes.proximoJogar.textContent = "Game over: Velha";
+            return;
+        }
+        opcoes.proximoJogar.textContent = `Game over: Jogador ${vencedor} vence!`;
+    }
+
+    function intialState(){
+        return [[null, null, null], 
+                [null, null, null], 
+                [null, null, null]]
+    }
+
+    function player(board){
+        let count = 0;
+        for (array of board){
+            count += array.filter(x => x).length
+        }
+        return (count % 2 ? O : X);
+    }
+
+    function actions(board){
+        let actionSet = new Set();
+        
+        for ([x, i] of board.entries()){
+            for([y, j] of i.entries()){
+                if (!j){
+                actionSet.add([x, y])
+                }
+            }
+        }
+        return actionSet;
+    }
+
+    function result(board, action){
+        let x = action[0];
+        let y = action[1];
+
+        currentPlayer = player(board);
+        let boardCopy = JSON.parse(JSON.stringify(board))
+        boardCopy[x][y] = currentPlayer;
+        return boardCopy;
+    }
+
+    function winner(board){
+
+        let estrategias = [ [board[0][0], board[0][1], board[0][2]],
+                            [board[1][0], board[1][1], board[1][2]],
+                            [board[2][0], board[2][1], board[2][2]],
+                            [board[0][0], board[1][0], board[2][0]],
+                            [board[0][1], board[1][1], board[2][1]],
+                            [board[0][2], board[1][2], board[2][2]],
+                            [board[0][0], board[1][1], board[2][2]],
+                            [board[0][2], board[1][1], board[2][0]] ]   
+        
+        for (estrategia of estrategias){
+            if (estrategia.filter(x => x == estrategia[0]).length == 3 && estrategia[0])
+                return estrategia[0];
+        }
+
+        return null;   
+    }
+
+    function terminal(board){
+        if (winner(board) || board.toString().length == 17)
+            return true;
+        
+        return false;
+    }
+
+    function utility(board){
+        let final = winner(board);
+
+        if (!final)
+            return 0
+        
+        return (final == X ? 1 : -1)
+    }
+
+    //Funções AI
+    function minimax(board){
+        if (terminal(board))
+            return null;
+        
+        let bestAction = null;
+        let alpha = -Infinity;
+        let beta = Infinity;
+
+        let currentValue;
+        if (player(board) == X){
+            let bestValue = alpha;
+            for (action of actions(board)){
+                currentAction = action;
+                currentValue = minValue(result(board, action), alpha, beta);
+                if (currentValue > bestValue){
+                    bestValue = currentValue;
+                    bestAction = currentAction;
+                }
+            }
+            return bestAction;
+        }
+        else{
+            let bestValue = beta;
+            for (action of actions(board)){
+                currentAction = action;
+                currentValue = maxValue(result(board, action), alpha, beta);
+                if (currentValue < bestValue){
+                    bestValue = currentValue;
+                    bestAction = currentAction;
+                }
+            }
+            return bestAction;
+        }
+    }
+
+    function maxValue(board, alpha, beta){
+        if (terminal(board))
+            return utility(board)
+        
+        let value = -Infinity;
+
+        for (action of actions(board)){
+            value = Math.max(value, minValue(result(board, action), alpha, beta));
+            alpha = Math.max(alpha, value);
+            if (alpha >= beta)
+                break;
+        }
+        return value;
+    }
+
+    function minValue(board, alpha, beta){
+        if (terminal(board))
+            return utility(board);
+
+        let value = Infinity;
+
+        for (action of actions(board)){
+            value = Math.min(value, maxValue(result(board, action), alpha, beta));
+            beta = Math.min(beta, value);
+            if (beta <= alpha)
+                break
+        }
+        return value;
+    }
+
 })()
